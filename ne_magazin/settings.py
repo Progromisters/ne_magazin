d64d420c98823456cbcf230341988478b221d068
Altair <eclipsealtair@gmail.com>
2018-09-10 01:28:32 +0300

    db for signup and login

--- ne_magazin/settings.py
+++ ne_magazin/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'ne_magazin.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': [os.path.join(BASE_DIR, 'templates'), os.path.join(BASE_DIR, 'templates', 'accounts')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
