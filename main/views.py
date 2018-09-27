5815f0ee88d83a64d6e1725bdc4d0d505828b2cd
Altair <eclipsealtair@gmail.com>
2018-09-27 21:40:06 +0300

    livereload

--- main/views.py
+++ main/views.py
@@ -4,5 +4,9 @@ from .models import Product
 
 def home(request):
     products = Product.objects.filter(is_active=True)
+    
+    session_key = request.session.session_key
+    if not session_key:
+      request.session.cycle_key()
     return render(request, 'main/index.html', locals())
 
--- ne_magazin/settings.py
+++ ne_magazin/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'livereload',
     'django.contrib.staticfiles',
     'allauth',
     'allauth.account',
@@ -52,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'livereload.middleware.LiveReloadScript',
 ]
 
 ROOT_URLCONF = 'ne_magazin.urls'
