7b209ec22a96542894b4dd2de483c0d8cf42b994
LaDIp <laa2014@gmail.com>
2018-09-28 20:51:21 +0300

    Fixed add produts.

--- static/assets/js/basket.js
+++ static/assets/js/basket.js
@@ -1,44 +1,44 @@
-$(document).ready(function () {
-  var submit_btn = $('#submit_btn');
-  submit_btn.click(function () {
-    var product_id = submit_btn.data("product_id");
-    var product_name = submit_btn.data("product_name");
-    var product_price = submit_btn.data("product_price");
-    console.log(product_id);
-    console.log(product_name);
-    console.log(product_price);
+'use strict';
 
-    //
-    $('.basket-items ul').append(
-      '<li>'
-      + product_name + ' $' + product_price + ' ' + '<a class="delete-item" href="">x</a>' +
-      '</li>'
-    );
-
-  });
-
-
-  //корзина
-  function showingBasket(){
+let submit_btns = $('.submit_btn');
+for (let i = 0; i < submit_btns.length; i++){
+    let submit_btn = submit_btns[i];
+    submit_btn.onclick = () => {
+        let product_id = submit_btn.dataset.product_id;
+        let product_name = submit_btn.dataset.product_name;
+        let product_price = submit_btn.dataset.product_price;
+        console.log(product_id);
+        console.log(product_name);
+        console.log(product_price);
+        $('.basket-items ul').append(
+            '<li>'
+            + product_name + ' $' + product_price + ' ' + '<a class="delete-item fas fa-times" href=""></a>' +
+            '</li>'
+        );
+    };
+}
+//корзина
+function showingBasket(){
     $('.basket-items').toggleClass('hidden');
-  }
+}
 
-  $('.basket-container').on('click', function(e){
+$('.basket-container').on('click', function(e) {
     e.preventDefault();
     showingBasket();
-  });
-
-  $('.basket-container').mouseover(function(){
-    showingBasket();
-  });
-
-  $('.basket-container').mouseout(function(){
-    showingBasket();
-  });
-
-  //удаление товара
-  $(document).on('click', '.delete-item', function(e){
-         e.preventDefault();
-         $(this).closest('li').remove();
-     })
 });
+
+$('.basket-container').mouseover(function(){
+    showingBasket();
+});
+
+$('.basket-container').mouseout(function(){
+    showingBasket();
+});
+
+//удаление товара
+$(document).on('click', '.delete-item', function(e){
+    e.preventDefault();
+    $(this).closest('li').remove();
+    
+});
+
